921c6a90d8b06eab41100a3883842616132fdd65
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-06-06 00:13:43 +0300

    update Title Component

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styled from "styled-components";
+
+export const Title=()=> {
+
+    return(
+        <Profile>
+            Welcome to Healthy Life
+        </Profile>
+    )
+}
+
+const Profile = styled.div`
+  margin-bottom: 2vw;
+  font-size: 2rem;
+  font-family: "Fira Code Medium",monospace;
+`
